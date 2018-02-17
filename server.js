f725839c0ed87d4823106dd1ab47c7d5b23708e7
alfonsotech <alfonsotech@nym.hush.com>
2018-02-16 20:47:53 -0600

    Sets up React client and proxies server

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express')
 var app = express()
 var PORT = process.env.PORT || 5000
 
+app.use(express.static("client/build"));
+
 app.get('/', function (req, res) {
-  res.json({'message': 'Hello WOrld2'})
+  // res.json({'message': 'Hello WOrld2'})
+  res.sendFile(path.join(__dirname + 'client/build/index.html'));
 })
 
 app.listen(PORT, function() {
